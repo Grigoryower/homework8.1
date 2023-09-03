12a8ecd4c8cc3bad2d45ce5108aace0148f6cae5
Grigoryower <140705935+Grigoryower@users.noreply.github.com>
2023-09-03 13:39:53 +0300

    homework8.1.1

--- src/Main.java
+++ src/Main.java
@@ -11,41 +11,29 @@ public class Main {
 
     public static void leapYear(int year) {
         System.out.println("Задание 1");
-
-        boolean leapYear = false;
-        if (year % 4 == 0) {
-            leapYear = true;
-            if (year % 100 == 0) {
-                if (year % 400 == 0)
-                    leapYear = true;
-                else
-                    leapYear = false;
-            }
-        } else
-            leapYear = false;
-        if (!leapYear)
-            System.out.println(year + " : год — невисокосный год");
-        else
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " : год — високосный год");
+        } else
+            System.out.println(year + " : год — невисокосный год");
     }
 
     public static void task1() {
-        leapYear(2017);
+        leapYear(2005);
 
     }
 
-    public static void clientDeviceYear(int currentYear,int clientOS) {
-        if (currentYear < 2015 &&clientOS == 0 ) {
-        System.out.printf("Установите новую версию приложения для Ois\n");}
-            else if (clientOS == 0 && currentYear >= 2015 ) {
-          System.out.printf(" Установите облегченную версию приложения для Ois\n");
-            }
-         else if (currentYear >= 2015 && clientOS == 1) {
-                System.out.printf("Установите облегченную версию приложения для Android \n");}
-             else if (clientOS == 1 && currentYear < 2015 ) {
-              System.out.printf("Установите новую версию приложения для Android\n");
-           }
+    public static void clientDeviceYear(int currentYear, int clientOS) {
+        if (currentYear < 2015 && clientOS == 0) {
+            System.out.printf("Установите новую версию приложения для Ois\n");
+        } else if (clientOS == 0 && currentYear >= 2015) {
+            System.out.printf(" Установите облегченную версию приложения для Ois\n");
+        } else if (currentYear >= 2015 && clientOS == 1) {
+            System.out.printf("Установите облегченную версию приложения для Android \n");
+        } else if (clientOS == 1 && currentYear < 2015) {
+            System.out.printf("Установите новую версию приложения для Android\n");
+        }
     }
+
     public static void task2() {
         System.out.println("Задание 2");
         clientDeviceYear(2015, 1);
@@ -53,21 +41,22 @@ public class Main {
     }
 
     public static String deliveryDistanceDays(int deliveryDistance) {
-        int day= 1;
+        int day = 1;
         if (deliveryDistance <= 20) {
         }
         if (deliveryDistance > 20 && deliveryDistance <= 60) {
             day++;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            day= day + 2;
+            day = day + 2;
 
-        } else if (deliveryDistance > 100){
-           return ("Свыше 100 км доставки нет ");
+        } else if (deliveryDistance > 100) {
+            return ("Свыше 100 км доставки нет ");
         }
-        return "Займет  дней на доставку "  + day;
+        return "Займет  дней на доставку " + day;
     }
+
     public static void task3() {
         System.out.println("Задание 3");
-        System.out.println( deliveryDistanceDays(61));
+        System.out.println(deliveryDistanceDays(61));
     }
 }
